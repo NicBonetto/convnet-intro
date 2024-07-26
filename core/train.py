c236a7d2296a9761535c608f10d906e80f2229fc
Nic Bonetto <28014739+NicBonetto@users.noreply.github.com>
2024-07-26 14:09:58 -0700

    fix: corrected syntax errors

--- core/train.py
+++ core/train.py
@@ -6,7 +6,7 @@ from keras.utils import to_categorical
 def train_model():
     # Create model network
     model = models.Sequential()
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape(28, 28, 1)))
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Conv2D(64, (3, 3), activation='relu'))
     model.add(layers.MaxPooling2D((2, 2)))
@@ -18,7 +18,7 @@ def train_model():
     model.add(layers.Dense(10, activation='softmax'))
 
     # Define training/test data
-    (train_images, train_labels), (test_images, test_labels) = mnsit.load_data()
+    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
     train_images = train_images.reshape((60000, 28, 28, 1))
     train_images = train_images.astype('float32') / 255
